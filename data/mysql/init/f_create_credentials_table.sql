210478436de000695d8303fd458809b2ae989fcc
WFAGuy.com <37864434+wfaguy@users.noreply.github.com>
2022-09-15 09:58:20 +0200

    dbtype 10 long

--- data/mysql/init/f_create_credentials_table.sql
+++ data/mysql/init/f_create_credentials_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE `credentials` (
   `port` int(11) DEFAULT NULL,
   `description` text NOT NULL,
   `secure` tinyint(4) DEFAULT NULL,
-  `db_type` varchar(250) DEFAULT NULL,  
+  `db_type` varchar(10) DEFAULT NULL,  
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_AnsibleForms_credentials_natural_key` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
