67137e79a02df05cae798966eebf66120113cea5
vcmirko <37864434+wfaguy@users.noreply.github.com>
2023-03-20 17:15:28 +0100

    aws with credentials

--- data/mysql/init/h_create_awx_table.sql
+++ data/mysql/init/h_create_awx_table.sql
@@ -2,7 +2,9 @@ USE `AnsibleForms`;
 DROP TABLE IF EXISTS `awx`;
 CREATE TABLE `awx` (
   `uri` varchar(250) NOT NULL,
-  `token` varchar(250) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `token` text NOT NULL,
+  `password` text NOT NULL,
   `ignore_certs` tinyint(4) DEFAULT NULL,
   `ca_bundle` text DEFAULT NULL  
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- data/mysql/init/z_insert_default_values.sql
+++ data/mysql/init/z_insert_default_values.sql
@@ -1,6 +1,6 @@
 USE `AnsibleForms`;
 INSERT INTO AnsibleForms.groups(name) VALUES('admins');
-INSERT INTO AnsibleForms.awx(uri,token) VALUES('','');
+INSERT INTO AnsibleForms.awx(uri,token,username,password) VALUES('','','','');
 INSERT INTO AnsibleForms.users(username,password,group_id) VALUES('admin','$2b$10$Z/W0HXNBk2aLR4yVLkq5L..C8tXg.G.o1vkFr8D2lw8JSgWRCNiCa',1);
 INSERT INTO AnsibleForms.ldap(server,port,ignore_certs,enable_tls,cert,ca_bundle,bind_user_dn,bind_user_pw,search_base,username_attribute,enable) VALUES('',389,1,0,'','','','','','sAMAccountName',0);
 INSERT INTO AnsibleForms.settings(mail_server,mail_port,mail_secure,mail_username,mail_password,mail_from,url) VALUES('',25,0,'','','','');
