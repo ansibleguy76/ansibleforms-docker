93b45d5d7a43b53a5d3bd37bfcb3b706fb0ef671
Mirko Van Colen <mirko@vancolen.com>
2024-08-15 13:25:53 +0200

    bug fixes

--- data/mysql/init/j.create_settings_table.sql
+++ data/mysql/init/j.create_settings_table.sql
@@ -7,5 +7,6 @@ CREATE TABLE `settings` (
   `mail_username` varchar(250) DEFAULT NULL,
   `mail_password` text DEFAULT NULL,
   `mail_from` varchar(250) DEFAULT NULL,
-  `url` varchar(250) DEFAULT NULL
+  `url` varchar(250) DEFAULT NULL,
+  `forms_yaml` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,    
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
