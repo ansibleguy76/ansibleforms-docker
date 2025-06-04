c2e6a5de0c2b4a9e018f903d27b54e0fe1336f2d
ansibleguy76 <info@ansibleguy.com>
2025-06-04 12:24:52 +0200

    logger bug

--- data/functions/custom.js
+++ data/functions/custom.js
@@ -1,14 +1,14 @@
 const logger=require("../lib/logger");
 
 // Add you custom functions here.  You can add the logger optionally
-// logger supports .error .warn .info .debug .silly
+// logger supports .error .warn .info .notice .debug
 // the functions do not need to start with fn, but we all like consistancy
 
 exports.fnSum = function(a,b) {
-  logger.silly("[fnSum] sum is happening")
+  logger.debug("[fnSum] sum is happening")
   return a+b
 };
 exports.fnMultiply = function(a,b) {
-  logger.silly("[fnMultiply] multiply is happening")
+  logger.debug("[fnMultiply] multiply is happening")
   return a*b
 };
