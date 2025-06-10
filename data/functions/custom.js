6a926657a85377023ebad4e450b5a765fe561b5a
ansibleguy76 <info@ansibleguy.com>
2025-06-10 18:48:06 +0200

    new custom.js file with export default

--- data/functions/custom.js
+++ data/functions/custom.js
@@ -1,14 +1,33 @@
-const logger=require("../lib/logger");
+import logger from "../lib/logger.js";
+import dns from "dns";
 
 // Add you custom functions here.  You can add the logger optionally
-// logger supports .error .warn .info .notice .debug
+// logger supports .error .warn .info .debug .silly
 // the functions do not need to start with fn, but we all like consistancy
 
-exports.fnSum = function(a,b) {
+const fnSum = function(a,b) {
   logger.debug("[fnSum] sum is happening")
   return a+b
 };
-exports.fnMultiply = function(a,b) {
+const fnMultiply = function(a,b) {
   logger.debug("[fnMultiply] multiply is happening")
   return a*b
+};
+const fnDnsResolve = async function(hostname,type) {
+  logger.debug("[fnDnsResolve] dns resolve is happening")
+  return new Promise((resolve,reject) => {
+    dns.resolve(hostname,type,(err,addresses) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(addresses)
+      }
+    })
+  })
+}
+
+export default {
+  fnSum,
+  fnMultiply,
+  fnDnsResolve
 };
